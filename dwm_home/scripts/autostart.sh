161b125f9f5af89773a20c94ac99248b95cd0b81
kzfj <hkrazer@hotmail.com>
2020-11-19 16:06:14 +0800

    dwm 改底部状态栏

--- dwm_home/scripts/autostart.sh
+++ dwm_home/scripts/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/bin/bash ~/scripts/dwm-status.sh &
+#/bin/bash ~/scripts/dwm-status.sh &
 #/bin/bash ~/scripts/tap-to-click.sh &
 #/bin/bash ~/scripts/inverse-scroll.sh &
 #/bin/bash ~/scripts/wp-autochange.sh &
@@ -14,5 +14,5 @@ fcitx &
 xfce4-volumed-pulse &
 #/bin/bash ~/scripts/run-mailsync.sh &
 #/bin/bash ~/scripts/setxmodmap-colemak.sh &
-trayer --edge top --align right --width 5 --height 16 --transparent true --tint 0x88888888
+trayer --edge bottom --align right --width 5 --height 16 --transparent true --tint 0x88888888
 ~/scripts/autostart_wait.sh &
